228e831002547f89bd29111d76437d1a3e2e9b92
Juliana Mota <julianamota89@gmail.com>
2019-04-24 11:45:59 +0200

    Updates

---
+++ precache-manifest.2c7f76cb3c4df41b81323cdcdb63a6e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17637fee6fbd2e374a68",
+    "url": "/react-tweet/static/css/main.8eefd537.chunk.css"
+  },
+  {
+    "revision": "17637fee6fbd2e374a68",
+    "url": "/react-tweet/static/js/main.1d2b93ad.chunk.js"
+  },
+  {
+    "revision": "86bd336a1cb5c9a6994c",
+    "url": "/react-tweet/static/js/runtime~main.1bd38ead.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-tweet/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "abcab031aa5578dd38e339205acc92b1",
+    "url": "/react-tweet/index.html"
+  }
+];
